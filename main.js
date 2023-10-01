c99e5dc6303804361f71359d0175b12ac0ce1b7f
Tauhid Patel <tauhidpatel18@gmail.com>
2023-10-01 19:01:26 +0530

    feat: adding random color bg divs functionality

---
+++ main.js
@@ -0,0 +1,34 @@
+// Gloabl
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+
+//Functions
+
+// color generator
+function generateHex() {
+    // const letters = '#0123456789ABCDEF';
+    // let hash = '#';
+    
+    // for(let i=0; i<6; i++) {
+    //     hash += letters[Math.floor(Math.random() * 16)];
+    // }
+    // return hash;
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+// random color divs on the page
+function randomColors() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+
+        // add color to the background
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+    });
+}
+randomColors();
