ec4a1eaac7065f74cf1ddefeddd6d604e8f9cb0d
Tauhid Patel <tauhidpatel18@gmail.com>
2023-10-02 20:29:45 +0530

    feat: slider inputs, hue, sat, brihghtness

--- main.js
+++ main.js
@@ -30,9 +30,19 @@ function randomColors() {
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
         checkTextContrast(randomColor, hexText); // checks for contrast
+
+        //initialize colorize sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll(".sliders input");
+        console.log(sliders);
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
     });
 }
-randomColors();
+
 function checkTextContrast(color, text) {
     const luminance = chroma(color).luminance(); // any color format, normal, hex or rgb
     if(luminance > 0.5) {
@@ -42,3 +52,20 @@ function checkTextContrast(color, text) {
     }
 }
 
+function colorizeSliders(color, hue, brightness, saturation) {
+    // scale saturation
+    const noSat = color.set('hsl.s', 0);
+    const fullSat = color.set('hsl.s', 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+    // scale brightness
+    const midBright = color.set('hsl.l', 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+    // update input colors
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)})`
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204, 204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75))`;
+}
+
+randomColors();
